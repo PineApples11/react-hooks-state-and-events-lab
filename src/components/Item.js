caefa9fb66e003f779267b513b3535875cf03c21
PineApples11 <kinyanjuiivy1@gmail.com>
2025-01-19 10:37:35 +0300

    practise on states and events

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React,{useState} from "react";
+import items from "../data/items";
 
 function Item({ name, category }) {
+  const [isInCart,setIsInCart]=useState(false)
+
+function handleCartClick(){
+  setIsInCart((prevState) => !prevState);
+}
+
+const itemClass = isInCart? "in-cart": "";
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={isInCart? "remove":"add"} onClick={handleCartClick}>
+        {isInCart? "Remove from cart":"Add to cart"}</button>
     </li>
   );
 }
